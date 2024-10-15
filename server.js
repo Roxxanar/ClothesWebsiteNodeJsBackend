4bebee1ea0ba889254ee3a54493f12e58a143f23
Roxana <mara-roxana.rusu@student.tuiasi.ro>
2024-10-15 14:55:00 +0300

    allclothesnew-works

--- server.js
+++ server.js
@@ -44,6 +44,60 @@ app.get('/clothing', (req, res) => {
   });
 });
 
+// Create an API endpoint to fetch users
+app.get('/allclothesnew', (req, res) => {
+
+  const query = `
+    SELECT 
+      ac.ID AS AllClothesID,
+      ac.IsNew,
+      ac.IsAtSale,
+      c.ID AS ClothingID,
+      c.Name AS ClothingName,
+      c.Photo AS ClothingPhoto,
+      c.Price AS ClothingPrice,
+      c.Color AS ClothingColor,
+      c.Size AS ClothingSize,
+
+      s.ID AS ShoesID,
+      s.Name AS ShoesName,           -- Changed alias
+      s.Photo AS ShoesPhoto,         -- Changed alias
+      s.Price AS ShoesPrice,         -- Changed alias
+      s.Color AS ShoesColor,         -- Changed alias
+      s.Size AS ShoesSize,           -- Changed alias
+    
+      b.ID AS BagsID,
+      b.Name AS BagsName,            -- Changed alias
+      b.Photo AS BagsPhoto,          -- Changed alias
+      b.Price AS BagsPrice,          -- Changed alias
+      b.Color AS BagsColor,          -- Changed alias
+      b.Size AS BagsSize,            -- Changed alias
+    
+      a.ID AS AccessoriesID,
+      a.Name AS AccessoriesName,     -- Changed alias
+      a.Photo AS AccessoriesPhoto,   -- Changed alias
+      a.Price AS AccessoriesPrice,    -- Changed alias
+      a.Color AS AccessoriesColor,    -- Changed alias
+      a.Size AS AccessoriesSize       -- Changed alias
+
+    FROM AllClothes ac
+    LEFT JOIN ClothingID c ON ac.ClothingID = c.ID
+    LEFT JOIN ShoesID s ON ac.ShoesID = s.ID
+    LEFT JOIN BagsID b ON ac.BagsID = b.ID
+    LEFT JOIN AccessoriesID a ON ac.AccessoriesID = a.ID
+    WHERE ac.IsNew = 1;
+  `;
+
+
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 // Start the server
 const port = 3000;
 app.listen(port, () => {
