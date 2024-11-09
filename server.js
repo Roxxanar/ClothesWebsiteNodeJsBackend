3073706b6420855159fbc1c554d0b96e24fa7028
Roxana <mara-roxana.rusu@student.tuiasi.ro>
2024-11-09 13:43:26 +0200

    first-commit

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-
 const express = require('express');
 const mysql = require('mysql');
-const cors = require('cors');
+const { ApolloServer, gql } = require('apollo-server-express');
+const jwt = require('jsonwebtoken');
 
+// Initialize Express and MySQL
 const app = express();
+const SECRET_KEY = 'your_secret_key';
+
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:4200', // Allow requests from Angular frontend
+}));
 
 
-const corsOptions = {
-  origin: 'http://localhost:4200', // The address of the Angular frontend
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-
-
-app.use(express.json());
-
-// Create a connection to MySQL database
+// MySQL Database Connection
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root', // Default user, change if necessary
-  password: '', // Default password, change if necessary
-  database: 'mango' // Name of your database
+  user: 'root',
+  password: '',
+  database: 'mango'
 });
 
-// Connect to the database
 db.connect(err => {
   if (err) {
     console.error('Database connection failed: ' + err.stack);
@@ -33,73 +29,185 @@ db.connect(err => {
   console.log('Connected to MySQL');
 });
 
-// Create an API endpoint to fetch users
-app.get('/clothing', (req, res) => {
-  db.query('SELECT * FROM ClothingID', (err, results) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(results);
+// GraphQL Schema
+const typeDefs = gql`
+  type Clothing {
+    ID: Int!
+    Name: String
+    Photo: String
+    Price: Float
+    Color: String
+    Size: String
+  }
+
+  type AllClothes {
+    AllClothesID: Int!
+    IsNew: Boolean
+    IsAtSale: Boolean
+    Clothing: Clothing
+    Shoes: Clothing
+    Bags: Clothing
+    Accessories: Clothing
+  }
+
+  type Query {
+    allClothing: [Clothing]
+    allClothesNew: [AllClothes]
+    protectedData: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): String
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    allClothing: async () => {
+      const query = 'SELECT * FROM Clothing';
+      return new Promise((resolve, reject) => {
+        db.query(query, (err, results) => {
+          if (err) reject(err);
+          else resolve(results);
+        });
+      });
+    },
+
+    allClothesNew: async () => {
+      const query = `
+        SELECT 
+          ac.ID AS AllClothesID,
+          ac.IsNew,
+          ac.IsAtSale,
+          c.ID AS ClothingID,
+          c.Name AS ClothingName,
+          c.Photo AS ClothingPhoto,
+          c.Price AS ClothingPrice,
+          c.Color AS ClothingColor,
+          c.Size AS ClothingSize,
+          s.ID AS ShoesID,
+          s.Name AS ShoesName,
+          s.Photo AS ShoesPhoto,
+          s.Price AS ShoesPrice,
+          s.Color AS ShoesColor,
+          s.Size AS ShoesSize,
+          b.ID AS BagsID,
+          b.Name AS BagsName,
+          b.Photo AS BagsPhoto,
+          b.Price AS BagsPrice,
+          b.Color AS BagsColor,
+          b.Size AS BagsSize,
+          a.ID AS AccessoriesID,
+          a.Name AS AccessoriesName,
+          a.Photo AS AccessoriesPhoto,
+          a.Price AS AccessoriesPrice,
+          a.Color AS AccessoriesColor,
+          a.Size AS AccessoriesSize
+        FROM AllClothes ac
+        LEFT JOIN ClothingID c ON ac.ClothingID = c.ID
+        LEFT JOIN ShoesID s ON ac.ShoesID = s.ID
+        LEFT JOIN BagsID b ON ac.BagsID = b.ID
+        LEFT JOIN AccessoriesID a ON ac.AccessoriesID = a.ID
+        WHERE ac.IsNew = 1;
+      `;
+      return new Promise((resolve, reject) => {
+        db.query(query, (err, results) => {
+          if (err) reject(err);
+          else resolve(
+            results.map(row => ({
+              AllClothesID: row.AllClothesID,
+              IsNew: row.IsNew,
+              IsAtSale: row.IsAtSale,
+              Clothing: {
+                ID: row.ClothingID,
+                Name: row.ClothingName,
+                Photo: row.ClothingPhoto,
+                Price: row.ClothingPrice,
+                Color: row.ClothingColor,
+                Size: row.ClothingSize,
+              },
+              Shoes: {
+                ID: row.ShoesID,
+                Name: row.ShoesName,
+                Photo: row.ShoesPhoto,
+                Price: row.ShoesPrice,
+                Color: row.ShoesColor,
+                Size: row.ShoesSize,
+              },
+              Bags: {
+                ID: row.BagsID,
+                Name: row.BagsName,
+                Photo: row.BagsPhoto,
+                Price: row.BagsPrice,
+                Color: row.BagsColor,
+                Size: row.BagsSize,
+              },
+              Accessories: {
+                ID: row.AccessoriesID,
+                Name: row.AccessoriesName,
+                Photo: row.AccessoriesPhoto,
+                Price: row.AccessoriesPrice,
+                Color: row.AccessoriesColor,
+                Size: row.AccessoriesSize,
+              },
+            }))
+          );
+        });
+      });
+    },
+
+    protectedData: (parent, args, context) => {
+      if (!context.user) {
+        throw new Error("Authentication required");
+      }
+      return "This is protected data accessible only to authenticated users.";
+    },
+  },
+
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await new Promise((resolve, reject) => {
+        db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {
+          if (err) reject(err);
+          else resolve(results[0]);
+        });
+      });
+
+      if (!user) {
+        throw new Error('Invalid credentials');
+      }
+
+      return jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '1h' });
     }
-  });
-});
+  },
+};
 
-// Create an API endpoint to fetch users
-app.get('/allclothesnew', (req, res) => {
+// Context function for authentication
+const context = ({ req }) => {
+  const token = req.headers.authorization || '';
+  try {
+    const user = jwt.verify(token.replace('Bearer ', ''), SECRET_KEY);
+    return { user };
+  } catch (error) {
+    return {};
+  }
+};
 
-  const query = `
-    SELECT 
-      ac.ID AS AllClothesID,
-      ac.IsNew,
-      ac.IsAtSale,
-      c.ID AS ClothingID,
-      c.Name AS ClothingName,
-      c.Photo AS ClothingPhoto,
-      c.Price AS ClothingPrice,
-      c.Color AS ClothingColor,
-      c.Size AS ClothingSize,
+// Initialize Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers, context });
+//await server.start();
+//server.applyMiddleware({ app });
 
-      s.ID AS ShoesID,
-      s.Name AS ShoesName,           -- Changed alias
-      s.Photo AS ShoesPhoto,         -- Changed alias
-      s.Price AS ShoesPrice,         -- Changed alias
-      s.Color AS ShoesColor,         -- Changed alias
-      s.Size AS ShoesSize,           -- Changed alias
-    
-      b.ID AS BagsID,
-      b.Name AS BagsName,            -- Changed alias
-      b.Photo AS BagsPhoto,          -- Changed alias
-      b.Price AS BagsPrice,          -- Changed alias
-      b.Color AS BagsColor,          -- Changed alias
-      b.Size AS BagsSize,            -- Changed alias
-    
-      a.ID AS AccessoriesID,
-      a.Name AS AccessoriesName,     -- Changed alias
-      a.Photo AS AccessoriesPhoto,   -- Changed alias
-      a.Price AS AccessoriesPrice,    -- Changed alias
-      a.Color AS AccessoriesColor,    -- Changed alias
-      a.Size AS AccessoriesSize       -- Changed alias
-
-    FROM AllClothes ac
-    LEFT JOIN ClothingID c ON ac.ClothingID = c.ID
-    LEFT JOIN ShoesID s ON ac.ShoesID = s.ID
-    LEFT JOIN BagsID b ON ac.BagsID = b.ID
-    LEFT JOIN AccessoriesID a ON ac.AccessoriesID = a.ID
-    WHERE ac.IsNew = 1;
-  `;
-
-
-  db.query(query, (err, results) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(results);
-    }
-  });
-});
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
 // Start the server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`GraphQL Server is running on http://localhost:${port}${server.graphqlPath}`);
 });
+
+startServer();
